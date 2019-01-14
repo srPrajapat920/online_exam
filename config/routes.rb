eb5152f0e4fc6580f7afd168a67bc71122cdddfb
Sanajy rathore <35648538+srPrajapat920@users.noreply.github.com>
2019-01-14 17:37:04 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root to: "users#new"
   get '/exm/:id', to: 'tests#exm'
   get '/qset', to: 'tests#questionset'
   get '/qsetshow/:id', to: 'tests#qsetshow'
   get '/ques', to: 'tests#ques'
   get '/admin', to: 'sessions#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
 
 
